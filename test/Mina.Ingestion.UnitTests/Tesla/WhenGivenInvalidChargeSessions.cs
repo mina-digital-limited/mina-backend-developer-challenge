ee9550b44abfc8f0b67d027dd89ed330f420b552
Elliot Chaim <elliot@mina.co.uk>
2021-09-20 10:55:17 +0100

    invalid charge session tests

---
+++ test/Mina.Ingestion.UnitTests/Tesla/WhenGivenInvalidChargeSessions.cs
@@ -0,0 +1,179 @@
+using System;
+using Mina.Ingestion.Tesla;
+using Xunit;
+
+namespace Mina.Ingestion.UnitTests.Tesla
+{
+    [Collection(Collection.TeslaChargeSessionIngestionService)]
+    public class WhenGivenInvalidChargeSessions
+    {
+        private readonly TeslaChargeSessionIngestionService _sut;
+
+        public WhenGivenInvalidChargeSessions(TeslaChargeSessionIngestionServiceTestFixture fixture)
+        {
+            _sut = fixture.Sut;
+        }
+
+        [Fact]
+        public void ShouldRejectNullDescription()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = null,
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = "2021-08-17T04:30:00Z",
+                    ConsumptionWh = "28000",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectEmptyDescription()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "   \n   ",
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = "2021-08-17T04:30:00Z",
+                    ConsumptionWh = "28000",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectNullStartTime()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = null,
+                    EndTime = "2021-08-17T04:30:00Z",
+                    ConsumptionWh = "28000",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectEmptyStartTime()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = "   \n   ",
+                    EndTime = "2021-08-17T00:30:00Z",
+                    ConsumptionWh = "28000",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectNullEndTime()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = null,
+                    ConsumptionWh = "28000",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectEmptyEndTime()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = "   \n   ",
+                    ConsumptionWh = "28000",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectNullConsumption()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = "2021-08-17T00:30:00Z",
+                    ConsumptionWh = null,
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectEmptyConsumption()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = "2021-08-17T00:30:00Z",
+                    ConsumptionWh = "   \n   ",
+                },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => _sut.Ingest(input));
+        }
+
+        [Fact]
+        public void ShouldRejectNonDecimalConsumption()
+        {
+            var input = new[]
+            {
+                new TeslaChargeSession
+                {
+                    SessionId = "",
+                    Description = "Private charge for 34F555D2",
+                    StartTime = "2021-08-17T00:30:00Z",
+                    EndTime = "2021-08-17T00:30:00Z",
+                    ConsumptionWh = "this is not a decimal",
+                },
+            };
+
+            Assert.Throws<ArgumentException>(() => _sut.Ingest(input));
+        }
+    }
+}
